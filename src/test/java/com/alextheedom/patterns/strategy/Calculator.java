0fe0c668fc4af64d3ddf9396634579f561556a7e
Alex Theedom <alex@indigococo.com>
2015-07-26 09:57:25 +0100

    First commit

---
+++ src/test/java/com/alextheedom/patterns/strategy/Calculator.java
@@ -0,0 +1,23 @@
+package com.alextheedom.patterns.strategy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by atheedom on 26/07/15.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class Calculator {
+
+
+    @Test
+    public void test() {
+        assertThat(true);
+    }
+
+
+}
