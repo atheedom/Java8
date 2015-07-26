5c61625293de9d1d7cf9f845a8a6ad0aabe9a4c9
Alex Theedom <alex@indigococo.com>
2015-07-26 10:01:40 +0100

    Change test class name and add calculator class

---
+++ src/main/java/com/alextheedom/patterns/strategy/Calculator.java
@@ -0,0 +1,7 @@
+package com.alextheedom.patterns.strategy;
+
+/**
+ * Created by atheedom on 26/07/15.
+ */
+public class Calculator {
+}
--- src/test/java/com/alextheedom/patterns/strategy/CalculatorTest.java
+++ src/test/java/com/alextheedom/patterns/strategy/CalculatorTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 
 @RunWith(MockitoJUnitRunner.class)
-public class Calculator {
+public class CalculatorTest {
 
 
     @Test
